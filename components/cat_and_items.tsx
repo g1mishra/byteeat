ade8a1b1003b09986576270d637368b6d7733bbe
GeekyA <anshchadha9211@gmail.com>
2024-03-23 12:59:40 +0530

    added accordion to menu

--- components/cat_and_items.tsx
+++ components/cat_and_items.tsx
@@ -3,7 +3,21 @@
 import React from "react"
 import { CaretSortIcon } from "@radix-ui/react-icons"
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+  CardHeader,
+  CardFooter
+} from "@/components/ui/card"
 import {
   Collapsible,
   CollapsibleContent,
@@ -11,26 +25,26 @@ import {
 } from "@/components/ui/collapsible"
 
 const CatAndItems = ({ menu }: any) => {
-  console.log(menu)
+  // console.log(menu)
   return (
-    <div>
-      {Object.keys(menu).map((category) => (
-        <Collapsible key={category}>
-          <CollapsibleTrigger asChild>
-            <Button variant="ghost" size="lg">
-              {category}
-              <CaretSortIcon className="h-4 w-4" />
-            </Button>
-          </CollapsibleTrigger>
-          <CollapsibleContent>
-            <ul>
-            {menu[category].map(
-              (item) => <li key={item.dish}>{item.dish} - {item.price}</li>
-            )}
-            </ul>
-          </CollapsibleContent>
-        </Collapsible>
-      ))}
+    <div className="gap-y-120 content-center">
+      <Accordion type="single" collapsible className="w-full">
+        {Object.keys(menu).map((category) => (
+          
+          <AccordionItem value={category}>
+              <AccordionTrigger className="text-lg font-bold">{category}</AccordionTrigger>
+            {menu[category].map((item) => (
+              <AccordionContent>
+                <h2>
+                  {item.dish}
+                </h2>
+                <p></p>
+                <p>{item.price}</p>
+              </AccordionContent>
+            ))}
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   )
 }
