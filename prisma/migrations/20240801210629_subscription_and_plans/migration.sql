2086d4cb55228a90158c04bf6538921b4365b294
Jeevan Kumar <g1mishra.dev@gmail.com>
2024-08-02 02:41:13 +0530

    added plan and subscription modal

---
+++ prisma/migrations/20240801210629_subscription_and_plans/migration.sql
@@ -0,0 +1,157 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `total` on the `Order` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(10,2)`.
+  - The `status` column on the `Order` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - You are about to drop the column `item` on the `OrderItem` table. All the data in the column will be lost.
+  - You are about to alter the column `price` on the `OrderItem` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(10,2)`.
+  - You are about to alter the column `price` on the `PriceItemMap` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(10,2)`.
+  - A unique constraint covering the columns `[token]` on the table `verificationtokens` will be added. If there are existing duplicate values, this will fail.
+  - Made the column `email` on table `users` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('PENDING', 'ACCEPTED', 'DELIVERED', 'CANCELLED');
+
+-- CreateEnum
+CREATE TYPE "PlanType" AS ENUM ('STARTER', 'PRO');
+
+-- CreateEnum
+CREATE TYPE "SubscriptionStatus" AS ENUM ('ACTIVE', 'CANCELED', 'EXPIRED');
+
+-- DropForeignKey
+ALTER TABLE "Item" DROP CONSTRAINT "Item_categoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ItemCategory" DROP CONSTRAINT "ItemCategory_restaurantId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_restaurantId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderItem" DROP CONSTRAINT "OrderItem_orderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "PriceItemMap" DROP CONSTRAINT "PriceItemMap_itemId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "SocialLinks" DROP CONSTRAINT "SocialLinks_restaurantId_fkey";
+
+-- AlterTable
+ALTER TABLE "Item" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true;
+
+-- AlterTable
+ALTER TABLE "Order" ALTER COLUMN "total" SET DATA TYPE DECIMAL(10,2),
+DROP COLUMN "status",
+ADD COLUMN     "status" "OrderStatus" NOT NULL DEFAULT 'PENDING',
+ALTER COLUMN "createdAt" SET DATA TYPE TIMESTAMPTZ,
+ALTER COLUMN "updatedAt" SET DATA TYPE TIMESTAMPTZ;
+
+-- AlterTable
+ALTER TABLE "OrderItem" DROP COLUMN "item",
+ALTER COLUMN "price" SET DATA TYPE DECIMAL(10,2);
+
+-- AlterTable
+ALTER TABLE "PriceItemMap" ALTER COLUMN "price" SET DATA TYPE DECIMAL(10,2),
+ALTER COLUMN "portion" SET DEFAULT '';
+
+-- AlterTable
+ALTER TABLE "Restaurant" ADD COLUMN     "createdAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true,
+ADD COLUMN     "updatedAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "sessions" ALTER COLUMN "expires" SET DATA TYPE TIMESTAMPTZ;
+
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "email" SET NOT NULL,
+ALTER COLUMN "email_verified" SET DATA TYPE TIMESTAMPTZ;
+
+-- AlterTable
+ALTER TABLE "verificationtokens" ALTER COLUMN "expires" SET DATA TYPE TIMESTAMPTZ;
+
+-- CreateTable
+CREATE TABLE "Plan" (
+    "id" TEXT NOT NULL,
+    "type" "PlanType" NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DECIMAL(10,2) NOT NULL,
+    "interval" TEXT NOT NULL,
+    "currency" TEXT NOT NULL DEFAULT 'INR',
+    "isDefault" BOOLEAN NOT NULL DEFAULT false,
+    "features" JSONB NOT NULL,
+
+    CONSTRAINT "Plan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Subscription" (
+    "id" TEXT NOT NULL,
+    "restaurantId" TEXT NOT NULL,
+    "planType" "PlanType" NOT NULL,
+    "startDate" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "endDate" TIMESTAMPTZ,
+    "status" "SubscriptionStatus" NOT NULL DEFAULT 'ACTIVE',
+    "freeTrialEndDate" TIMESTAMPTZ,
+
+    CONSTRAINT "Subscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Plan_type_key" ON "Plan"("type");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subscription_restaurantId_key" ON "Subscription"("restaurantId");
+
+-- CreateIndex
+CREATE INDEX "Subscription_status_idx" ON "Subscription"("status");
+
+-- CreateIndex
+CREATE INDEX "Item_dish_idx" ON "Item"("dish");
+
+-- CreateIndex
+CREATE INDEX "Order_status_idx" ON "Order"("status");
+
+-- CreateIndex
+CREATE INDEX "Restaurant_slug_idx" ON "Restaurant"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verificationtokens_token_key" ON "verificationtokens"("token");
+
+-- AddForeignKey
+ALTER TABLE "ItemCategory" ADD CONSTRAINT "ItemCategory_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Item" ADD CONSTRAINT "Item_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "ItemCategory"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PriceItemMap" ADD CONSTRAINT "PriceItemMap_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "Item"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SocialLinks" ADD CONSTRAINT "SocialLinks_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "Item"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_planType_fkey" FOREIGN KEY ("planType") REFERENCES "Plan"("type") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- Add starter plan
+INSERT INTO "Plan" (id, type, name, description, price, interval, currency, "isDefault", features)
+VALUES
+  ('644acc43-34a5-4512-9618-67330fe39cf7', 'STARTER', 'Starter Plan', 'This is the Starter Plan with digital menu only.', 2999, 'year', 'INR', false, '["DIGITAL_MENU"]');
+
+-- Add pro plan
+INSERT INTO "Plan" (id, type, name, description, price, interval, currency, "isDefault", features)
+VALUES
+  ('06e9fea5-422b-45be-aaee-0ff87434ff9a', 'PRO', 'Pro Plan', 'This is the Pro Plan with all features.', 4999, 'year', 'INR', false, '["DIGITAL_MENU", "ORDERING", "RESERVE_TABLE"]');
