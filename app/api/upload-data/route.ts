db21755530ce00d79d6c40c61b23735557380f08
Jeevan Kumar <g1mishra.dev@gmail.com>
2024-10-28 01:11:57 +0530

    build error

--- app/api/upload-data/route.ts
+++ app/api/upload-data/route.ts
@@ -1,13 +1,12 @@
-import { insertData } from "@/temp/data-upload"
+// import { insertData } from "@/temp/data-upload"
 import { NextResponse } from "next/server"
-
 
 export async function GET(request: Request, context: any) {
   try {
-    const restaurantId = new URL(request.url).searchParams.get("restaurantId")
-    const data = await insertData(restaurantId || "")
-    return NextResponse.json({ data })
-    // return NextResponse.json({ data: "GET" })
+    // const restaurantId = new URL(request.url).searchParams.get("restaurantId")
+    // const data = await insertData(restaurantId || "")
+    // return NextResponse.json({ data })
+    return NextResponse.json({ data: "GET" })
   } catch (error) {
     console.error("Error uploading data: ", error)
     return NextResponse.json({ error: "Error uploading data" }, { status: 500 })
