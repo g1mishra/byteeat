3a5d76823feb2348d5c721acc15423459b7937a1
Jeevan Kumar <g1mishra.dev@gmail.com>
2024-11-12 02:28:38 +0530

    on-boarding added

--- app/manage/layout.tsx
+++ app/manage/layout.tsx
@@ -2,10 +2,6 @@ import { Metadata } from "next"
 import { redirect } from "next/navigation"
 import { getServerSession } from "next-auth"
 
-import RestroSidebar, {
-  SidebarItemType,
-} from "@/components/manage/RestroSidebar"
-
 import { authOptions } from "../api/auth/authOption"
 
 export const metadata: Metadata = {
@@ -13,29 +9,11 @@ export const metadata: Metadata = {
   description: "Manage your restaurant - menu, tables, and more",
 }
 
-const sidebarItems: SidebarItemType[] = [
-  {
-    href: `/manage`,
-    icon: "HomeIcon",
-    label: "Manage",
-    exact: true,
-  },
-  {
-    href: "/manage/orders",
-    icon: "ShoppingBagIcon",
-    label: "Orders",
-  },
-]
-
 interface SettingsLayoutProps {
   children: React.ReactNode
 }
 
-const HIDE_ON = ["/manage/restaurant"]
-
-export default async function SettingsLayout({
-  children,
-}: SettingsLayoutProps) {
+export default async function SettingsLayout({ children }: SettingsLayoutProps) {
   const serverSession = await getServerSession(authOptions)
 
   if (!serverSession) {
@@ -46,13 +24,5 @@ export default async function SettingsLayout({
     return redirect("/onboarding")
   }
 
-  return (
-    <RestroSidebar
-      sidebarItems={sidebarItems}
-      hideOn={HIDE_ON}
-      showLast={false}
-    >
-      {children}
-    </RestroSidebar>
-  )
+  return <>{children}</>
 }
