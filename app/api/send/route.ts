63f9ddee160b7837e9a953102bca927f8dac3af3
Jeevan Kumar <g1mishra.dev@gmail.com>
2024-11-14 12:31:57 +0530

    fix: update Resend API key environment variable

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,7 +2,7 @@ import { Resend } from "resend"
 
 import { EmailTemplate } from "@/components/email-template"
 
-const resend = new Resend(process.env.RESEND_API_KEY2)
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(request: Request) {
   try {
